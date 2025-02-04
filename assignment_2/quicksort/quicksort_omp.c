53556017033ded7c525b09b9647a34a5852f64e0
Emanuel Paraschiv <40666117+emyemy89@users.noreply.github.com>
2025-02-04 17:21:01 +0100

    final version

--- assignment_2/quicksort/quicksort_omp.c
+++ assignment_2/quicksort/quicksort_omp.c
@@ -5,8 +5,9 @@
 #include <stdio.h>
 #include <omp.h>
 
-#define MAXWORKERS 10
+#define MAXWORKERS 30
 #define MAXSIZE 1000000
+#define THRESHOLD 1000
 
 int threadCount = 0;
 
@@ -28,49 +29,25 @@ int swap(int *a, int *b);
 
 
 
-/* timer */
-// double read_timer() {
-//     static bool initialized = false;
-//     static struct timeval start;
-//     struct timeval end;
-//     if( !initialized )
-//     {
-//         gettimeofday( &start, NULL );
-//         initialized = true;
-//     }
-//     gettimeofday( &end, NULL );
-//     return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
-// }
-
-// void quicksort (int arr[], int len){
-//     #pragma omp parallel
-//     {
-//         #pragma omp single
-//         {
-//             quicksortrec(arr, 0, len - 1);
-//         }
-//     }
-// }
 
 
 
  void quicksort(int arr[], int min, int max) {
     if(min<max) {
         int pivot= split(arr,min,max);
-        #pragma omp parallel sections
-        {
-            // Left partition in parallel if within thread limit
-            #pragma omp parallel shared(arr) if( omp_get_num_threads() < nb_workers)
+
+        if ((max - min) < THRESHOLD) {
+            quicksort(arr, min, pivot - 1);
+            quicksort(arr, pivot + 1, max);
+        }else {
+            #pragma omp task shared(arr)
             quicksort(arr, min, pivot - 1);
 
-            // Right partition in parallel if within thread limit
-            #pragma omp parallel shared(arr) if( omp_get_num_threads() < nb_workers)
+            #pragma omp task shared(arr)
             quicksort(arr, pivot + 1, max);
 
-            // Synchronize tasks
-            //#pragma omp taskwait
+            #pragma omp taskwait
         }
-
     }
 }
 
@@ -112,11 +89,11 @@ int main(int argc, char *argv[]) {
 
     omp_set_num_threads(nb_workers);
 
-    //
-    // // Dynamically allocate memory for the array
+
+    // // memory for the array
     int* arr = (int*)malloc(size * sizeof(int));
 
-    // // Initialize the array with random values
+    // // random values
     for (int i = 0; i < size; i++) {
         arr[i] = rand() % 10000;
     }
@@ -124,7 +101,14 @@ int main(int argc, char *argv[]) {
 
     double start = omp_get_wtime();
 
-    quicksort(arr, 0, size-1);
+   // quicksort(arr, 0, size-1);
+
+    #pragma omp parallel
+     {
+        #pragma omp single
+        quicksort(arr, 0, size - 1);
+     }
+
     double end = omp_get_wtime();
 
 
@@ -135,6 +119,6 @@ int main(int argc, char *argv[]) {
 
     printf("\nExecution time: %f seconds\n", end - start);
 
-
+     free(arr);
     return 0;
 }
