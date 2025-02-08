1f8fbb63bbf3652ba36a5bcc6812296a17b6ab95
Mohammad Asfour <mohammad.asfour0743@gmail.com>
2025-02-08 18:12:17 +0100

    Finished bees and bear problem

--- assignment_3/BeesBear/beesBear.c
+++ assignment_3/BeesBear/beesBear.c
@@ -12,23 +12,25 @@
 #include <windows.h>
 #include <semaphore.h>
 
-#define PRODUCERS 2
+#define PRODUCERS 5
 #define CAPACITY 10
 
 int H; //Honey pot
-
+int capac;
 
 sem_t mutex; //lock
 sem_t full_pot; //signaled when pot is full
 
+//threads will enter, if pot is still under capacity it will get the lock and add into the pot and sleep.
+//When pot is empty, a thread (that has the lock) will signal full_pot condition that is in _Bear and give the bear the lock mutex,
+//which will empty the pot and release the lock so the waiting threads can reaquire it and fill the pot again.
 void* _Bees(void* arg) {
-
-  int worker_id = * (int*)arg;
+  int worker_id = *((int*)arg);
+  free(arg); // Free dynamically allocated memory
   printf("Bee number %d is reporting for duty\n", worker_id);
-
   while(1) {
     sem_wait(&mutex);
-    if (H == 10) {
+    if (H >= CAPACITY) {
       printf("Bee number %d has filled the pot and waking up the bear\n", worker_id);
       sem_post(&full_pot);
     }
@@ -36,9 +38,8 @@ void* _Bees(void* arg) {
       printf("Bee number %d is putting one portion of honey in the pot\n", worker_id);
       H++;
       sem_post(&mutex);
-
-      //Sleep(rand() % 10);
     }
+    Sleep( 10);
   }
 }
 
@@ -50,27 +51,39 @@ void* _Bear() {
     printf("Bear has been woken and will eat the honey\n");
     H = 0;
     sem_post(&mutex);
+    Sleep( rand()%1000);
   }
 
 }
 
 int main(int argc, char *argv[]) {
 
+//Inputs from command line:
+  //First integer is number of bees. If nothing is specified then it is 5.
+  //Second integer is capacity of honey pot. If nothing is specified then it is 10.
+
+
   H  = 0;
-  int numWorkers = 2;
 
+  sem_init(&mutex, 0, 1); //Initialize lock
+  sem_init(&full_pot, 0, 0);//Initialize condition
 
+  int numWorkers = (argc > 1) ? atoi(argv[1]) : PRODUCERS; //Gets number of threads from command line. If not, use PRODUCERS
+  capac = (argc > 1) ? atoi(argv[2]) : CAPACITY; //Gets number of threads from command line. If not, use PRODUCERS
+  if (numWorkers > PRODUCERS) numWorkers = PRODUCERS;
+  if (capac > CAPACITY) capac = CAPACITY;
 
   pthread_t bees[numWorkers];
   pthread_t bear;
 
-  sem_init(&mutex, 1, 1); //Initialize lock
-  sem_init(&full_pot, 1, 0);//Initialize condition
+  printf("Capacity of honey pot is %d\n", capac);
 
   printf("Program started\n");
   srand(time(NULL));
-  for (long l = 0; l < numWorkers; l++) {
-    pthread_create(&bees[l],NULL, _Bees, (void *) l);
+  for (int l = 0; l < numWorkers; l++) {
+    int* id = malloc(sizeof(int)); // Allocate memory for thread argument
+    *id = l; // Unique worker ID
+    pthread_create(&bees[l], NULL, _Bees, id);
   }
   pthread_create(&bear, NULL, _Bear, NULL);
 
@@ -80,6 +93,8 @@ int main(int argc, char *argv[]) {
   }
 
 
-  printf("Programs terminated\n");
+  sem_destroy(&mutex);
+  sem_destroy(&full_pot);
+  return 0;
 
 }
