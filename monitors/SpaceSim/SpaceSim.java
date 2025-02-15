c9eb15209de600f4d1276aa8936b1b7adda967fb
Mohammad Asfour <mohammad.asfour0743@gmail.com>
2025-02-15 16:45:26 +0100

    First implementation of the space fuel sim

---
+++ monitors/SpaceSim/SpaceSim.java
@@ -0,0 +1,78 @@
+import java.util.Random;
+
+public class SpaceSim {
+
+  public static void main(String[] args) {
+    int V = 3; //number of concurrent vehicles
+    int N = 500; //max nitrogen
+    int Q = 500; //max quantum fluid
+
+    Station station = new Station(V, N, Q);
+
+    int number_of_vehicles = 5;
+    int number_of_suppliers = 2;
+
+    for (int i = 0; i < number_of_vehicles; i++) {
+      new SpaceV(station, 100, 100).start();
+    }
+
+    for (int i = 0; i < number_of_suppliers; i++) {
+      new SupplyV(station, 350, 400).start();
+    }
+  }
+
+  //Thread that refuels from station
+  static class SpaceV extends Thread {
+
+    private Station station;
+    int N_requested, Q_requested;
+    int nb_of_visits;
+
+    private Random random;
+
+    public SpaceV(Station station, int N_requested, int Q_requested) {
+      this.station = station;
+      this.N_requested = N_requested;
+      this.Q_requested = Q_requested;
+      this.random = new Random();
+    }
+
+    @Override
+    public void run() {
+      while (nb_of_visits < 5) {
+        station.request_fuel(N_requested, Q_requested); //get fuel
+        nb_of_visits++;
+        try { //sleep
+          Thread.sleep(random.nextInt(2000));
+        } catch (InterruptedException e) {}
+        
+      }
+    }
+  }
+
+  //Thread that brings fuel to the station
+  static class SupplyV extends Thread {
+
+    private Station station;
+    int N_supplied, Q_supplied;
+    private Random random;
+
+    public SupplyV(Station station, int N_supplied, int Q_supplied) {
+      this.station = station;
+      this.N_supplied = N_supplied;
+      this.Q_supplied = Q_supplied;
+      this.random = new Random();
+    }
+
+    @Override
+    public void run() {
+      station.refuel_station(N_supplied, Q_supplied); //supply fuel
+
+      station.request_fuel(random.nextInt(100), random.nextInt(100)); //get fuel
+
+      try { //sleep
+        Thread.sleep(random.nextInt(4000));
+      } catch (InterruptedException e) {}
+    }
+  }
+}
---
+++ monitors/SpaceSim/Station.java
@@ -0,0 +1,80 @@
+import java.util.Random;
+
+
+
+
+//Monitor
+public class Station{ 
+    int V; //max nb of Vehicles that can be served
+    int N; //max nb of nitrogen that can be stored
+    int Q; //max nb of quantum fluid that can be stored
+
+    int N_available, Q_available , V_available;
+    Random random = new Random();
+
+    public Station(int V, int N, int Q){
+        this.V = V;
+        this.N = N;
+        this.Q = Q;
+
+        V_available = V;
+        N_available = N;
+        Q_available = Q;
+       
+    }
+
+
+    public void request_fuel(int requested_N, int requesed_Q){
+
+        synchronized(this){
+            while( V_available == 0 || requested_N > N_available || requesed_Q > Q_available){
+                try {
+                    wait();
+                } catch (InterruptedException e) {}
+            }
+
+            V_available--;
+            N_available -= requested_N;
+            Q_available -= requesed_Q;
+            System.out.println("Thread " + Thread.currentThread().getName() + " has recieved " + requested_N + " Nitrogen and " + requesed_Q + " quantum fuel");
+        }
+        try {
+            Thread.sleep(random.nextInt(500));
+        } catch (InterruptedException e) {}
+
+        leave_station();
+        
+
+
+    }
+
+    public synchronized void leave_station(){
+        
+            V_available++;
+            notifyAll();
+            System.out.println("Thread " + Thread.currentThread().getName() + " has left the station");
+        
+    }
+
+    public synchronized void refuel_station(int N_supplied, int Q_supplied){
+        
+            while( N_available + N_supplied > N || Q_available + Q_supplied > Q){
+                try {
+                    wait();
+                } catch (InterruptedException e) {}
+ 
+            }
+
+            N_available += N_supplied;
+            Q_available += Q_supplied;
+            System.out.println("Supply thread " + Thread.currentThread().getName() + " has supplied " + N_supplied + " Nitrogen and " + Q_supplied + " quantum fuel");
+            notifyAll();
+            leave_station();
+
+    }
+
+}
+
+
+
+
