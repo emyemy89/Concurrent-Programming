1b35c5a864ab9bd56d112180d09d46df4d89ad67
Mohammad Asfour <mohammad.asfour0743@gmail.com>
2025-02-22 13:25:56 +0100

    finished task3

--- message_passing_interface/Philosophers/philosophers.c
+++ message_passing_interface/Philosophers/philosophers.c
@@ -12,20 +12,26 @@
 #define RELEASE 2
 #define APPROVED 3
 #define DENIED 4
+#define DONE 5
 
 
 void server(){
   int forks[5] = {1, 1, 1, 1, 1}; //1 is available, 0 is not
   int request_type; //can be REQUEST (1) or RELEASE (2)
   int response_type;
+  int philo_finished = 0;
   MPI_Status status;
   int first = 0;
   int second = 0;
 
-  while (1) {
+  while (philo_finished < 5) {
     MPI_Recv(&request_type, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
     int philo_rank = status.MPI_SOURCE;
-    //if message tag is REQUEST,
+
+    if (request_type == DONE) {
+      philo_finished++;
+      continue;
+    }
 
     if ((philo_rank % 2)==0){ //if rank is even
       first = philo_rank - 1; //left fork
@@ -50,8 +56,7 @@ void server(){
       forks[first] = forks[second] = 1;
     }
   }
-
-  }
+}
 
 
 
@@ -66,14 +71,12 @@ void server(){
 
 
 
-  //TASK:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-  //make philosophers eat 5 times then exit program
-
   MPI_Status status;
   int message, response;
+  int count = 3;
   srand(time(NULL));
 
-  while (1) {
+  while (count > 0) {
     //Think
     sleep(rand() % 4);
 
@@ -89,14 +92,18 @@ void server(){
     }
 
     printf("Philosopher %d has acquired forks and started eating\n", rank);
-    sleep(rand() % 4);
+
+    sleep(rand() % 2);
 
     printf("Philosopher %d is releasing forks\n", rank);
     message = RELEASE;
     MPI_Send(&message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    count--;
 
   }
-
+  printf("Philosopher %d has finished eating........................................................................\n", rank);
+  message = DONE;
+  MPI_Send(&message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
 }
 
 
