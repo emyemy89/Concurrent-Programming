83d0216c8c70cf74edf3fd6fb9f8075074944d5e
Mohammad Asfour <mohammad.asfour0743@gmail.com>
2025-02-22 13:52:28 +0100

    removed complicated logic

--- message_passing_interface/Philosophers/philosophers.c
+++ message_passing_interface/Philosophers/philosophers.c
@@ -10,67 +10,59 @@
 
 #define REQUEST 1
 #define RELEASE 2
-#define APPROVED 3
-#define DENIED 4
-#define DONE 5
+#define DONE 3
+#define APPROVED 4
+#define DENIED 5
 
 
+
+//get request from philosopher
+//if message is RELEASE, release forks
+//if message is DONE, increment conter
+//if message is REQUEST, check if left and right forks are available
+//if yes, set this to 0 (give them to philosopher) and send APPROVED
+//if no, send DENIED message
 void server(){
   int forks[5] = {1, 1, 1, 1, 1}; //1 is available, 0 is not
-  int request_type; //can be REQUEST (1) or RELEASE (2)
+  int request_type; //can be REQUEST (1), RELEASE (2), DONE (3)
   int response_type;
   int philo_finished = 0;
   MPI_Status status;
-  int first = 0;
-  int second = 0;
+  int left = 0;
+  int right = 0;
 
   while (philo_finished < 5) {
     MPI_Recv(&request_type, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
     int philo_rank = status.MPI_SOURCE;
-
     if (request_type == DONE) {
       philo_finished++;
       continue;
     }
-
-    if ((philo_rank % 2)==0){ //if rank is even
-      first = philo_rank - 1; //left fork
-      second = philo_rank % 5; //right fork
-    }
-    else if ((philo_rank % 2)==1){ //if rank is odd
-      first = philo_rank % 5; //right fork
-      second = philo_rank - 1; //left fork
-    }
-
+    left = philo_rank - 1; //left fork
+    right = philo_rank % 5; //right fork
     if (request_type == REQUEST) {
-      if (forks[first] && forks[second]) {
-        forks[first] = forks[second] = 0; //lock corresponding forks
+      if (forks[left] && forks[right]) {
+        forks[left] = forks[right] = 0; //lock corresponding forks
         response_type = APPROVED;
       }
       else {response_type = DENIED;}
-
       MPI_Send(&response_type, 1, MPI_INT, philo_rank, 0, MPI_COMM_WORLD);
-
     }
     else if (request_type == RELEASE) {
-      forks[first] = forks[second] = 1;
+      forks[left] = forks[right] = 1;
     }
   }
 }
 
 
-
+//think for random time
+//request fork at i and i+1 to eat by sending request
+//wait for approval using recv
+//if request not approved, retry after thinking
+//if request approved, eat for rand amount of time then release forks by sending request
+//use a counter so each philosopher will eat n (3) times before returning
+//when finished eating the philospher sends a message to server that it is done
  void philosopher(int rank){
-
-   //think for random time
-   //request fork at i and i+1 to eat by sending request
-   //wait for approval using recv
-   //if request not approved, retry using infinite while loop
-   //if request approved, eat for rand amount of time then release forks by sending request
-   //use a counter so each philosopher will eat n times before returning
-
-
-
   MPI_Status status;
   int message, response;
   int count = 3;
@@ -79,7 +71,6 @@ void server(){
   while (count > 0) {
     //Think
     sleep(rand() % 4);
-
 
     printf("Philosopher %d is requesting forks\n", rank);
     message = REQUEST;
@@ -92,14 +83,13 @@ void server(){
     }
 
     printf("Philosopher %d has acquired forks and started eating\n", rank);
-
+    //eat
     sleep(rand() % 2);
 
     printf("Philosopher %d is releasing forks\n", rank);
     message = RELEASE;
     MPI_Send(&message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     count--;
-
   }
   printf("Philosopher %d has finished eating........................................................................\n", rank);
   message = DONE;
