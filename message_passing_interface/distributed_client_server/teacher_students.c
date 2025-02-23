7fa05be0d4e1af3c2ddf3a364fec17e16e8a8cf7
emyemy89 <emiparaschiv345@gmail.com>
2025-02-23 17:07:44 +0100

    minor changes

--- message_passing_interface/distributed_client_server/teacher_students.c
+++ message_passing_interface/distributed_client_server/teacher_students.c
@@ -7,40 +7,35 @@ void teacher(int n) {
     int arr[n];
     MPI_Status status;
 
-    // Request each student's rank one by one
+    // Request each student rank one by one
     for (int i = 0; i < n; i++) {
-        //int request_signal = 1;
-       // MPI_Ssend(&request_signal, 1, MPI_INT, i + 1, 0, MPI_COMM_WORLD);
         MPI_Recv(&student_rank, 1, MPI_INT, i + 1, 0, MPI_COMM_WORLD, &status);
         arr[i] = student_rank;
     }
 
     // Pair students
     for (int i = 0; i < n - 1; i += 2) {
-        MPI_Ssend(&arr[i + 1], 1, MPI_INT, arr[i], 0, MPI_COMM_WORLD);
-        MPI_Ssend(&arr[i], 1, MPI_INT, arr[i + 1], 0, MPI_COMM_WORLD);
+        MPI_Send(&arr[i + 1], 1, MPI_INT, arr[i], 0, MPI_COMM_WORLD);
+        MPI_Send(&arr[i], 1, MPI_INT, arr[i + 1], 0, MPI_COMM_WORLD);
     }
 
-    // Handle the odd student if n is odd
+    // for odd number of students
     if (n % 2 == 1) {
-        MPI_Ssend(&arr[n - 1], 1, MPI_INT, arr[n - 1], 0, MPI_COMM_WORLD);
+        MPI_Send(&arr[n - 1], 1, MPI_INT, arr[n - 1], 0, MPI_COMM_WORLD);
     }
 }
 
 void students(int rank) {
     int partner;
     MPI_Status status;
-    //int request_signal;
 
-    // Wait for the teacher to request the rank
-    //MPI_Recv(&request_signal, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-    MPI_Ssend(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
 
     // Receive partner info from the teacher
     MPI_Recv(&partner, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 
-    // Print the result (flush ensures it prints immediately)
-    printf("Student %d is paired with Student %d\n", rank, partner);
+
+    printf("Student number %d is paired with student %d\n", rank, partner);
     fflush(stdout);
 }
 
@@ -50,7 +45,7 @@ int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    int n = size - 1; // Exclude teacher
+    int n = size - 1; //exclude teacher
 
     if (rank == 0) {
         teacher(n);
